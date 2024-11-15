12b4e50fb626ead55fabe5a12b8f890071668dc5
RobertoFloris <florisroberto99@gmail.com>
2024-11-15 14:59:58 +0100

    [fix] folder 1 es 4

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -24,7 +24,10 @@ teachers.pop();
 // console.log(lastTeacher)
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers[0];
+teachers.shift();
+// console.log(teachers)
+// console.log(firstTeacher)
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
